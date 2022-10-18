39a01148b206d7c3a21bece1b7fb64cd134a5805
vahidsaeid72 <vahid.sd.programmer@gmail.com>
2022-10-18 09:56:18 +0330

    navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,15 @@ import { RiCloseLine } from "react-icons/ri";
 import { SiAnaconda } from "react-icons/si";
 import './Navbar.css';
 import logo from "./../../assets/logo.png"
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [showIcon, setShowIcon] = useState(false);
+
     return (
         <nav className="navbar container">
             <div className="menu">
-                <ul className="Navbar-links">
+                <ul className={`Navbar-links ${showIcon ? "hide" : "show"}`}>
                     <li className="link-border">
                         اموزش بیشتر
                     </li>
@@ -30,7 +33,10 @@ const Navbar = () => {
             <div className="logo">
                 <img src={logo} alt="" />
             </div>
-        </nav>
+            <div className="menu-icon" onClick={() => setShowIcon(!showIcon)}>
+                {showIcon ? <AiOutlineBars size={35} /> : <RiCloseLine size={35} />}
+            </div>
+        </nav >
     );
 }
 
